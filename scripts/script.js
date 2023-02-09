2827e5d7ec180d7398ea58d172a1562d4c276364
Nikola <markovi3c.nikola@gmail.com>
2023-02-09 20:33:19 +0100

    Start making reusable function for each hero-button, finish tomorrow when well rested.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const transition = document.querySelector("#hero-main");
 const mainTitle = document.querySelector(".marquee");
 
+const heroTopLeft = document.querySelector("#hero-top-left");
+const heroTopRight = document.querySelector("#hero-top-right");
+const heroBottom = document.querySelector("#hero-bottom");
+
 transition.addEventListener("mouseover", function () {
   mainTitle.setAttribute("id", "ok");
 });
@@ -8,3 +12,10 @@ transition.addEventListener("mouseover", function () {
 transition.addEventListener("mouseout", function () {
   mainTitle.setAttribute("id", "ok1");
 });
+
+function changeHeroHover() {
+  this.setAttribute("id", "ok");
+  console.log(this);
+}
+
+heroBottom.addEventListener("mouseover", changeHeroHover);
